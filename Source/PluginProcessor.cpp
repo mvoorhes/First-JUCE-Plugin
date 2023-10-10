197d360d2431428fb5d15f38fc8583950852b1ee
mvoorhes <mvoorhes@ucsc.edu>
2023-10-10 11:30:33 -0700

    Added Parameters

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -166,7 +166,8 @@ bool FirstJUCEpluginAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* FirstJUCEpluginAudioProcessor::createEditor()
 {
-    return new FirstJUCEpluginAudioProcessorEditor (*this);
+//    return new FirstJUCEpluginAudioProcessorEditor (*this);
+    return new juce::GenericAudioProcessorEditor(*this);
 }
 
 //==============================================================================
@@ -182,6 +183,60 @@ void FirstJUCEpluginAudioProcessor::setStateInformation (const void* data, int s
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
 }
+
+// Code I've written
+juce::AudioProcessorValueTreeState::ParameterLayout FirstJUCEpluginAudioProcessor::createParameterLayout()
+{
+    juce::AudioProcessorValueTreeState::ParameterLayout layout;
+    
+    layout.add(
+        std::make_unique<juce::AudioParameterFloat>(
+            juce::ParameterID("LowCut Freq", 1),
+            "LowCut Freq",
+            juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 1.f),
+            20.f
+        ),
+        std::make_unique<juce::AudioParameterFloat>(
+           juce::ParameterID("HighCut Freq", 1),
+           "HighCut Freq",
+           juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 1.f),
+           20000.f
+        ),
+        std::make_unique<juce::AudioParameterFloat>(
+           juce::ParameterID("Peak Freq", 1),
+           "Peak Freq",
+           juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 1.f),
+           750.f
+        ),
+        std::make_unique<juce::AudioParameterFloat>(
+           juce::ParameterID("Peak Gain", 1),
+           "Peak Gain",
+           juce::NormalisableRange<float>(-24.f, 24.f, 0.5f, 1.f),
+           0.f
+        ),
+        std::make_unique<juce::AudioParameterFloat>(
+           juce::ParameterID("Peak Quality", 1),
+           "Peak Quality",
+           juce::NormalisableRange<float>(0.1f, 10.f, 0.05f, 1.f),
+           1.f
+        )
+    );
+    
+    juce::StringArray choices;
+    for (int i = 0; i < 4; i++) {
+        juce::String str;
+        str << (12 + i*12) << " db/Oct";
+        choices.add(str);
+    }
+    
+    layout.add(
+        std::make_unique<juce::AudioParameterChoice>(juce::ParameterID("LowCut Slope", 1), "LowCut Slope", choices, 0),
+        std::make_unique<juce::AudioParameterChoice>(juce::ParameterID("HighCut Slope", 1), "HighCut Slope", choices, 0)
+    );
+    
+    return layout;
+}
+
 
 //==============================================================================
 // This creates new instances of the plugin..
